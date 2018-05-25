658c60632308ac0db05416f538578dfb100f7365
learn <learn@learn04.local>
2018-05-25 13:41:02 -0700

    working on images

--- apartmentBackEnd/app/models/user.rb
+++ apartmentBackEnd/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  rolify
   has_secure_password # <-- This is the new line
+  has_many :grades
 end
--- apartmentBackEnd/config/routes.rb
+++ apartmentBackEnd/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   post 'user_token' => 'user_token#create'
   get 'user_token' => 'user_token#index'
+  root "grades#index"
   resources :apartments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
